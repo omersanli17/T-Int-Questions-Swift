f8c9e098091437ade10099cd371b22a690463ce6
Omer <sanli.omer@metu.edu.tr>
2024-02-18 14:08:59 +0300

    Add: Question 1

--- Interview/main.swift
+++ Interview/main.swift
@@ -7,5 +7,26 @@
 
 import Foundation
 
-print("Hello, World!")
+/*
+Have the function VowelCount (str) take the str string parameter being passed and
+return the number of vowels the string contains
+(ie. "All cows eat grass and moo" would return 8). Do not count y as a vowel for this
+ 
+ Ünlü harflerin sayısını return ettirme
+*/
+
+public func vowelCount(_ str: String) -> Int {
+    let vowels: Set<Character> = ["a", "e", "ı" ,"i", "o", "u", "ü"]
+    var count = 0
+
+    for char in str.lowercased() {
+        if vowels.contains(char) {
+            count += 1
+        }
+    }
+
+    return count
+}
+
+print(vowelCount("şemsi paşa pasajında sesi büzüşesiceler"))
 
